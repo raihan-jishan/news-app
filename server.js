6ee87438ab0b0fafe3c4453b06acdbaacf1c225c
raihan-jishan <112199871+raihan-jishan@users.noreply.github.com>
2023-05-25 16:07:35 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 // hello world example express
-const express = require("express"); // updated version
-const app = express() // .. app module scafholding
-const path = require("path"); // path module defined 
-const axios = require("axios"); // .. require axios 
-const port = 3000; // initailize one port
-// app methods
-app.get("/", (req, res) => {
- res.sendFile("index.html", { root: path.join(__dirname)});
-});
-// api route path with async function 
-app.get("/api", async(req, res) => {
-    let url = ' https://newsapi.org/v2/everything?' + req._parsedUrl.query
-    let r = await axios(url);
-    let a = r.data
-    res.json(a); 
-});
+// server files 
+const express = require('express')
+const path = require('path')
+const axios = require('axios')
+const app = express()
+const port = 8080
 
-//  app listen
+app.get('/', (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname) } )
+})
+
+app.get('/api', async(req, res) => {
+  console.log(req._parsedUrl.query)
+  let url = "https://newsapi.org/v2/everything?"+req._parsedUrl.query
+  let r = await axios(url)
+  let a = r.data 
+  res.json(a)
+})
+
 app.listen(port, () => {
-  console.log(`listening on http://localhost:${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
